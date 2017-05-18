730c5d038a66a9b75408f132f4465972913e1e57
tzits <tzitsman@gmail.com>
2017-05-18 16:13:04 -0400

    links table view to viewcontroller

--- emojidictionary/ViewController.swift
+++ emojidictionary/ViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableview: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableview.dataSource = self
+        tableview.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
