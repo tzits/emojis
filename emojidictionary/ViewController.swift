1128712d96fb01dcfa5293dbbb3cd6c70dfae222
tzits <tzitsman@gmail.com>
2017-05-19 05:10:05 -0400

    so slow

--- emojidictionary/ViewController.swift
+++ emojidictionary/ViewController.swift
@@ -12,13 +12,25 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     @IBOutlet weak var tableview: UITableView!
     
+    var emojis = ["🐱","🐧","🦊","🐊","🦁","🐳","🦉"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         tableview.dataSource = self
         tableview.delegate = self
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojis.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emojis[indexPath.row]
+        return cell
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
