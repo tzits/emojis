4de313534daab471cf3a591e8d0e184b8ce82221
tzits <tzitsman@gmail.com>
2017-05-19 14:13:47 -0400

    so many emojis

---
+++ DefintionViewController.swift
@@ -0,0 +1,24 @@
+//
+//  DefintionViewController.swift
+//  emojidictionary
+//
+//  Created by Toby Zitsman on 5/19/17.
+//  Copyright © 2017 Toby Zitsman. All rights reserved.
+//
+
+import UIKit
+
+class DefintionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
--- emojidictionary/ViewController.swift
+++ emojidictionary/ViewController.swift
@@ -31,6 +31,15 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let emoji = emojis[indexPath.row]
+        performSegue(withIdentifier: "moveSegue", sender: emoji)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        print(sender!)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
